affce6a7ad1e7cd90486c09458d56cc96c4889b9
SA,Shashank <Shashank.SA@Cerner.com>
2021-02-19 04:01:16 +0530

    Final_expect_editfeature

--- src/components/medicines-list.component.js
+++ src/components/medicines-list.component.js
@@ -1,11 +1,70 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios'
+
+const Medicine = props => (
+  <tr>
+    <td>{props.medicine.username}</td>
+    <td>{props.medicine.description}</td>
+    <td>{props.medicine.quantity}</td>
+    <td>{props.medicine.date.substring(0,10)}</td>
+    <td>
+      <Link to={"/edit/"+props.medicine._id}>edit</Link> | <a href="#" onClick={() => { props.deleteMedicine(props.medicine._id) }}>done</a>
+    </td>
+  </tr>
+)
 
 export default class MedicinesList extends Component {
+
+  constructor(props) {
+    super(props);
+    this.deleteMedicine = this.deleteMedicine.bind(this);
+    this.state = {medicines: []};
+  }
+
+  componentDidMount() {
+    axios.get('http://localhost:5000/medicines/')
+     .then(response => {
+       this.setState({ medicines: response.data });
+     })
+     .catch((error) => {
+        console.log(error);
+     })
+  }
+
+  deleteMedicine(id) {
+    axios.delete('http://localhost:5000/medicines/'+id)
+      .then(res => console.log(res.data));
+    this.setState({
+      medicines: this.state.medicines.filter(el => el._id !== id)
+    })
+  }
+
+  medicineList() {
+    return this.state.medicines.map(currentmedicine => {
+      return <Medicine medicine={currentmedicine} deleteMedicine={this.deleteMedicine} key={currentmedicine._id}/>;
+    })
+  }
+
   render() {
     return (
       <div>
-        <p>You are on the Medicines List component!</p>
-      </div>
+      <h3>Medicines Requested</h3>
+      <table className="table">
+        <thead className="thead-light">
+          <tr>
+            <th>Username</th>
+            <th>Description</th>
+            <th>Quantity</th>
+            <th>Date</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          { this.medicineList() }
+        </tbody>
+      </table>
+    </div>
     )
   }
 }
